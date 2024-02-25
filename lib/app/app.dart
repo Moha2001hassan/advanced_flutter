61fc0cd80fde4fb5a613da90721e1a16b9e64f73
Mohamed Hassan <mohahassanna933@gmail.com>
2024-02-25 17:30:24 +0200

    theme manager

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
+import '../presentation/resources/theme_manager.dart';
+
 class MyApp extends StatefulWidget {
 
   MyApp._internal();  // Private constructor
+
+  int appState = 0;
+
   static final MyApp _instance = MyApp._internal();  // Singleton instance variable
   factory MyApp() => _instance;  // Factory method to return the singleton instance
 
@@ -13,6 +18,8 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      theme: getApplicationTheme(),
+    );
   }
 }
